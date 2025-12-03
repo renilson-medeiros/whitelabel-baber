99e82bdcfb8e1098bf0f4616ca4c521ba627bbe0
Renilson Medeiros <renilson.medeiros96@outlook.com>
2025-12-03 17:54:50 -0300

    fix: Ajuste UX/UI Footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,12 +1,14 @@
 const Footer = () => {
   return (
     <footer className="bg-muted p-7">
-      <p className="text-foreground text-xs font-semibold">
-        &copy; 2025 Barber Cabeleireiro
-      </p>
-      <p className="text-muted-foreground text-xs">
-        Todos os direitos reservados.
-      </p>
+      <div className="container mx-auto px-0 sm:px-5">
+        <p className="text-foreground text-xs font-semibold">
+          &copy; 2025 Barber Cabeleireiro
+        </p>
+        <p className="text-muted-foreground text-xs">
+          Todos os direitos reservados.
+        </p>
+      </div>
     </footer>
   );
 };
